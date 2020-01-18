310de644edadcae8a00ca49850fd50fda734235f
Sergei Trofimovich <slyfox@gentoo.org>
2020-01-18 22:28:31 +0000

    setup.py: tweak whitespace
    Signed-off-by: Sergei Trofimovich <slyfox@gentoo.org>

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 import numpy
 
-setup(name='seekwatcher',
-        version="0.50",
-        cmdclass = {'build_ext': build_ext},
-      ext_modules=[
-          Extension('seekwatcher.rundata',
-          ['seekwatcher/rundata.pyx'],
-          include_dirs = [numpy.get_include(),'.']),
-          Extension('seekwatcher.blkparse',
-          ['seekwatcher/blkparse.pyx'],
-          include_dirs = [numpy.get_include(),'.'])
-          ],
-      scripts=['cmd/seekwatcher'],
-      packages=['seekwatcher'])
+setup(
+    name='seekwatcher',
+    version="0.50",
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension(
+            'seekwatcher.rundata',
+            ['seekwatcher/rundata.pyx'],
+            include_dirs = [numpy.get_include(),'.']),
+        Extension(
+            'seekwatcher.blkparse',
+            ['seekwatcher/blkparse.pyx'],
+            include_dirs = [numpy.get_include(),'.'])
+    ],
+    scripts=['cmd/seekwatcher'],
+    packages=['seekwatcher'])
